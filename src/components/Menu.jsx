41e5de0b981204e1ae3dedbe5f36419823d83ff6
Thiel <thielrodrigues28@gmail.com>
2022-03-22 22:28:12 -0300

    commit

---
+++ src/components/Menu.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Menu = () => {
+  return (
+    <div>
+        <ul>
+           <li><a href=""  >Pagina 1</a></li>
+           <li><a href=""  >Pagina 2</a></li>
+           <li><a href="" > Pagina 3</a></li>
+       </ul>
+    </div>
+  )
+}
+
+export default Menu
